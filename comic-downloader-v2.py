e3e50412f35add95a2cbfa99ada0be16aea24187
nxshen <michael7105@gmail.com>
2020-12-28 22:58:29 +0800

    update

--- comic-downloader-v2.py
+++ comic-downloader-v2.py
@@ -39,25 +39,30 @@ def wait_until_find_elements_by_xpath(driver, xpath, url):
                 ttl = 5
 
 def wait_until_find_element_by_xpath(driver, xpath, url):
+    LIMIT = 4
+    counter = 0
     ttl = 5
+    time.sleep(1)
     while True:
+        counter += 1
         try:
             # wait = WebDriverWait(driver, 10)
             # element = wait.until(EC.presence_of_all_element_located((By.XPATH, xpath)))
             element = driver.find_element_by_xpath(xpath)
             return element
         except Exception as e:
-            print('Retry:', ttl)
-            time.sleep(1)
-            ttl -= 1
-            # print('Error: wait_until_find_element_by_xpath()')
-            if ttl < 1:
-                logging.error(e)
+            if(counter >= LIMIT):
+                logging.error('Failed over 50 times, restarting the driver')
                 logging.error('last file: ' + full_name)
                 logging.error('last url: ' + url)
+                driver = driver_restart(driver, chromeDriver, driver.current_url)
+            print('Retry:', ttl)
+            ttl -= 1
+            time.sleep(1)
+            # print('Error: wait_until_find_element_by_xpath()')
+            if ttl < 1:
                 driver.get(url)
                 ttl = 5
-
 
 def goto_next_page_or_chapter(driver, full_name, fptr):
     ttl = 5
@@ -89,14 +94,23 @@ def goto_next_page_or_chapter(driver, full_name, fptr):
                 fptr.close()
                 exit(0)
 
+def driver_restart(driver, chromeDriver, current_url):
+    driver.close()
+    driver = webdriver.Chrome(executable_path=chromeDriver)
+    driver.get(current_url)
+    try:
+        driver.find_element_by_xpath("//span[.='點擊此處繼續閱讀']").click()
+    except Exception as e:
+        pass
+    return driver
+
+
 # 基本設定、路徑等等都在這裡
 root_path = 'H:\野良神\\'
-# root_path = 'H:\無良公會\\'
-chapter_start_from = 51
+chapter_start_from = 75
 page_start_from = 41
 # 山立漫畫 - 你要下載的漫畫的首頁
 index_url = 'https://www.setnmh.com/comic-lpdaj-%E9%87%8E%E8%89%AF%E7%A5%9E'
-# index_url = 'https://www.setnmh.com/comic-lvcnh-%E7%84%A1%E8%89%AF%E5%85%AC%E6%9C%83'
 
 # 啟動chrome瀏覽器
 # chromedriver檔案放的位置，請自行下載 chromeDriver， google 搜尋 "chromeDriver" 即可，請下載當前電腦安裝的 chrome 版本的 Driver
